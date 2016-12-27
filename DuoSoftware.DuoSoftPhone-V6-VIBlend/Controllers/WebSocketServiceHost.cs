a47158d002425e06b82dc65bbbc2d7048c354780
waruna@duosoftware.com <waruna@duosoftware.com>
2016-12-27 15:16:04 +0530

    add to git

---
+++ DuoSoftware.DuoSoftPhone-V6-VIBlend/Controllers/WebSocketServiceHost.cs
@@ -0,0 +1,215 @@
+﻿using Alchemy;
+using Alchemy.Classes;
+using DuoSoftware.DuoSoftPhone.Controllers.Common;
+using DuoSoftware.DuoSoftPhone.Controllers.Service;
+using DuoSoftware.DuoTools.DuoLogger;
+using System;
+using System.Dynamic;
+using System.Net;
+using System.Security;
+using DuoSoftware.DuoSoftPhone.Controllers.AgentStatus;
+using Newtonsoft.Json.Linq;
+
+
+namespace DuoSoftware.DuoSoftPhone.Controllers
+{
+    
+    //delegate 
+    public delegate void SocketMessage(CallFunctions callFunction, string message);
+    /// <summary>
+    /// Socket Listener 
+    /// </summary>
+    public class WebSocketServiceHost
+    {
+
+        private static UserContext currentContext;
+        //event
+        public event SocketMessage OnRecive;
+
+        // key
+        private static string _duoKey;
+
+        /// <summary>
+        /// -- WebSocketServiceHost
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="securityToken"></param>
+        /// <param name="tenantId"></param>
+        /// <param name="companyId"></param>
+        public WebSocketServiceHost(int port)
+        {
+            try
+            {
+                _duoKey = Guid.NewGuid().ToString();
+                var server = new WebSocketServer(port, IPAddress.Any)
+                {
+                    OnConnected = OnConnected,
+                    OnDisconnect = OnDisconnected,
+                    OnSend = OnSend,
+                    OnConnect = OnConnect,
+                    OnReceive = OnRecieve,
+                    TimeOut = new TimeSpan(0, 5, 0)
+                };
+                
+                Console.WriteLine(server.ListenAddress);
+                server.Start();
+            }
+            catch (Exception exception)
+            {
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "WebSocketServiceHost", exception, Logger.LogLevel.Error);
+            }
+        }
+
+        /// <summary>
+        /// On Send
+        /// </summary>
+        /// <param name="aContext"></param>
+        /// <param name="message"></param>
+        private static void OnSend(UserContext aContext)
+        {
+            try
+            {
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, String.Format("OnSend : {0}", aContext.ClientAddress), Logger.LogLevel.Info);
+            }
+            catch (Exception exception)
+            {
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnSend", exception, Logger.LogLevel.Error);
+            }
+        }
+
+        private static void Reply(string message)
+        {
+            try
+            {
+                if (currentContext == null) return;
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, String.Format("Reply : {0}, message : {1}", currentContext.ClientAddress, message), Logger.LogLevel.Info);
+                currentContext.Send(message);
+            }
+            catch (Exception exception)
+            {
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "Reply", exception, Logger.LogLevel.Error);
+            }
+        }
+
+        private bool ValidateToken(string token)
+        {
+            try
+            {
+                return ardsHandler.ValidateToken(token);
+            }
+            catch (Exception exception)
+            {
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "validateToken", exception, Logger.LogLevel.Error);
+                return false;
+            }
+            
+        }
+        /// <summary>
+        /// On Receive 
+        /// </summary>
+        /// <param name="aContext"></param>
+        private void OnRecieve(UserContext aContext)
+        {
+            try
+            {
+
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnRecieve : " + aContext.ClientAddress, Logger.LogLevel.Info);
+                if (aContext.DataFrame == null) return;
+                var message = aContext.DataFrame.ToString();
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, string.Format("message : {0} form : {1}", message, aContext.ClientAddress), Logger.LogLevel.Info);
+
+                var callInfo = message.Split('|');// "token|callfunction|no|othr"
+                if (callInfo.Length != 4)
+                {
+                    Reply("Invalid Call Information's.");
+                    throw new FieldAccessException("Invalid Call Information's.");
+                }
+
+                var callFunction = (CallFunctions)Enum.Parse(typeof(CallFunctions), callInfo[1]);
+                if (callFunction == CallFunctions.Initiate)
+                {
+                    currentContext = aContext;
+                    if (ValidateToken(callInfo[0]))
+                    {
+                        _duoKey = Guid.NewGuid().ToString();
+                        dynamic expando = new JObject();
+                        expando.veery_api_key = _duoKey;
+                        Reply(expando.ToString());
+                    }
+                    else
+                    {
+                        Reply("Invalid SecurityToken or Expired.");
+                    }
+                    return;
+                }
+                if (!_duoKey.Equals(callInfo[0]))
+                {
+                    Reply("Invalid veery API Key.");
+                    throw new SecurityException("Invalid SecurityToken or Expired.");
+                }
+                
+                if (OnRecive != null)
+                    OnRecive(callFunction, callInfo[2]);
+                
+            }
+            catch (Exception exception)
+            {
+                Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnRecieve", exception, Logger.LogLevel.Error);
+            }
+        }
+
+        /// <summary>
+        /// On Connect
+        /// </summary>
+        /// <param name="aContext"></param>
+        private static void OnConnect(UserContext aContext)
+        {
+            Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnConnect : " + aContext.ClientAddress, Logger.LogLevel.Info);
+            //try
+            //{
+            //    Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnConnect : " + aContext.ClientAddress, Logger.LogLevel.Info);
+            //}
+            //catch (Exception exception)
+            //{
+            //    Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnConnect", exception, Logger.LogLevel.Error);
+            //}
+        }
+
+        /// <summary>
+        /// On Connected 
+        /// </summary>
+        /// <param name="aContext"></param>
+        private static void OnConnected(UserContext aContext)
+        {
+            
+            Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnConnected : " + aContext.ClientAddress, Logger.LogLevel.Info);
+            //try
+            //{
+            //    Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnConnected : " + aContext.ClientAddress, Logger.LogLevel.Info);
+            //}
+            //catch (Exception exception)
+            //{
+            //    Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnConnected", exception, Logger.LogLevel.Error);
+            //}
+        }
+
+        /// <summary>
+        /// on disconnect
+        /// </summary>
+        /// <param name="aContext"></param>
+        private static void OnDisconnected(UserContext aContext)
+        {
+            Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnDisconnected : " + aContext.ClientAddress, Logger.LogLevel.Info);
+            currentContext = null;
+            _duoKey = Guid.NewGuid().ToString();
+            //try
+            //{
+
+            //}
+            //catch (Exception exception)
+            //{
+            //    Logger.Instance.LogMessage(Logger.LogAppender.DuoLogger2, "OnDisconnected", exception, Logger.LogLevel.Error);
+            //}
+        }
+    }
+}
---
+++ DuoSoftware.DuoSoftPhone-V6-VIBlend/DuoCallTesterLicenseKey/LicenseKeyHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DuoCallTesterLicenseKey
+{
+    public class LicenseKeyHandler
+    {
+        private const string initVector = "tu89geji340t89u2";
+
+        // This constant is used to determine the keysize of the encryption algorithm.
+        private const int keysize = 256;
+        private static string cipherText = "pZkQANFF5pX9sYUO6SUTPnvmlZ108CUAShbyArW9lTlmPDiR7JgkT1ZqijXTswQCryYQHsEJ/3upLXIaURFS17eXlNuEK9x/vibpCqLR15zmqp6ilpCNR/IjRbG2AxeFimunVP/HGEZJOvtGxcbp9YaRz8pY/sp9+AJA1sTB7i6HYTZoRV5Olf7I9STNblrGXao//hIqEYfDOzi7gAaWWREKbsvH7RF/AGm9n0p+Zmn2FIbZAX0FfaxS/XW3uHL9";
+        public static string GetLicenseKey(string passPhrase)
+        {
+            try
+            {
+                byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+                byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+                PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+                MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
